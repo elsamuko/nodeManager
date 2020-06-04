a4b6e719b20045583635ac67f12b8cf0653ad924
Samuel Albrecht <elsamuko@gmail.com>
2020-06-04 11:26:32 +0200

    chg: draw line when in connect mode

--- src/ui/graphicsscene.cpp
+++ src/ui/graphicsscene.cpp
@@ -1,3 +1,29 @@
 #include "ui/graphicsscene.hpp"
 
+#include <QGraphicsLineItem>
+#include <QGraphicsSceneMouseEvent>
+
 GraphicsScene::GraphicsScene( QObject* parent ) : QGraphicsScene( parent ) {}
+
+void GraphicsScene::mousePressEvent( QGraphicsSceneMouseEvent* mouseEvent ) {
+    if( mode == Mode::Connect ) {
+        line = new QGraphicsLineItem( QLineF( mouseEvent->scenePos(),
+                                              mouseEvent->scenePos() ) );
+        addItem( line );
+    } else {
+        QGraphicsScene::mousePressEvent( mouseEvent );
+    }
+}
+
+void GraphicsScene::mouseMoveEvent( QGraphicsSceneMouseEvent* mouseEvent ) {
+    if( line && mode == Mode::Connect ) {
+        QLineF newLine( line->line().p1(), mouseEvent->scenePos() );
+        line->setLine( newLine );
+    } else {
+        QGraphicsScene::mouseMoveEvent( mouseEvent );
+    }
+}
+
+void GraphicsScene::mouseReleaseEvent( QGraphicsSceneMouseEvent* mouseEvent ) {
+    QGraphicsScene::mouseReleaseEvent( mouseEvent );
+}
--- src/ui/graphicsscene.hpp
+++ src/ui/graphicsscene.hpp
@@ -5,5 +5,20 @@
 class GraphicsScene : public QGraphicsScene {
         Q_OBJECT
     public:
+        enum class Mode { Move, Connect };
         explicit GraphicsScene( QObject* parent = nullptr );
+
+        //! toggles mouse mode between moving/connecting nodes
+        void setMode( Mode m ) { mode = m; }
+
+    protected:
+        void mousePressEvent( QGraphicsSceneMouseEvent* mouseEvent ) override;
+        void mouseMoveEvent( QGraphicsSceneMouseEvent* mouseEvent ) override;
+        void mouseReleaseEvent( QGraphicsSceneMouseEvent* mouseEvent ) override;
+
+    private:
+        //! temporary drawn line while connecting nodes
+        QGraphicsLineItem* line = nullptr;
+
+        Mode mode = Mode::Move;
 };
