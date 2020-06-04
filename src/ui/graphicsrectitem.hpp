7348eb9ae23bfe03769cb3accb467351a96eef7a
Samuel Albrecht <elsamuko@gmail.com>
2020-06-04 13:17:59 +0200

    add: use type() for GraphicsRectItem

--- src/ui/graphicsrectitem.hpp
+++ src/ui/graphicsrectitem.hpp
@@ -2,7 +2,10 @@
 
 #include <QGraphicsRectItem>
 
+#include "ui/graphicstypes.hpp"
+
 class GraphicsRectItem : public QGraphicsRectItem {
     public:
         explicit GraphicsRectItem( QGraphicsItem* parent = nullptr );
+        virtual int type() const override { return static_cast<int>( CustomGraphicsTypes::NodeType ); }
 };
