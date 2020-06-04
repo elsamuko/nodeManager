12e7c082ecc330338575fbff2e6acd4def560c5c
Samuel Albrecht <elsamuko@gmail.com>
2020-06-04 15:46:49 +0200

    add: return start/end ids for connections

--- src/ui/graphicslineitem.hpp
+++ src/ui/graphicslineitem.hpp
@@ -10,6 +10,8 @@ class GraphicsLineItem : public QGraphicsLineItem {
         explicit GraphicsLineItem( const qint64 id, GraphicsRectItem* startItem, GraphicsRectItem* endItem, QGraphicsItem* parent = nullptr );
         virtual int type() const override { return static_cast<int>( CustomGraphicsTypes::LineType ); }
         qint64 getId() const { return id; }
+        qint64 getIdStart() const { return startRect->getId(); }
+        qint64 getIdEnd() const { return endRect->getId(); }
 
     private:
         //! updates itself from new positions of startRect and endRect
