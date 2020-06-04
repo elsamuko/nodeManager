39aced180696689a70568e20791130edec9e1306
Samuel Albrecht <elsamuko@gmail.com>
2020-06-04 17:33:32 +0200

    add: comments

--- src/ui/node.hpp
+++ src/ui/node.hpp
@@ -10,11 +10,13 @@ class Node : public QGraphicsRectItem {
     public:
         explicit Node( const qint64 id, QGraphicsItem* parent = nullptr );
         virtual int type() const override { return static_cast<int>( CustomGraphicsTypes::NodeType ); }
+        //! add callback, which is called, if Node is moved
         void setOnMoveCallback( const QGraphicsItem* sender, const std::function<void()>& func ) { onMoveCallbacks.emplace( sender, func ); }
         void removeOnMoveCallback( const QGraphicsItem* sender ) { onMoveCallbacks.erase( sender ); }
         qint64 getId() const { return id; }
 
     protected:
+        //! \note calls onMoveCallbacks
         virtual QVariant itemChange( GraphicsItemChange change, const QVariant& value ) override;
 
     private:
